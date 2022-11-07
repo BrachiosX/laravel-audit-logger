b794f4cc73e36385e2226cd6f5d3ccf34c07bc8c
Liam <zawhlyan@gmail.com>
2022-11-08 03:29:32 +0630

    feat: add option to ignore auditing on certain action

---
+++ src/Enums/AuditAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BrachiosX\AuditLogger\Enums;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method static AuditAction CREATE()
+ * @method static AuditAction UPDATE()
+ * @method static AuditAction DELETE()
+ */
+final class AuditAction extends Enum
+{
+    private const Test = 'test';
+    private const CREATE = 'create';
+    private const UPDATE = 'update';
+    private const DELETE = 'delete';
+}
--- src/Traits/HasAuditLog.php
+++ src/Traits/HasAuditLog.php
@@ -2,6 +2,8 @@
 
 namespace BrachiosX\AuditLogger\Traits;
 
+use \Illuminate\Support\Collection;
+use BrachiosX\AuditLogger\Enums\AuditAction;
 use BrachiosX\AuditLogger\Jobs\OnCreatedLoggerJob;
 use BrachiosX\AuditLogger\Jobs\OnDeletedLoggerJob;
 use BrachiosX\AuditLogger\Jobs\OnUpdateLoggerJob;
@@ -10,20 +12,46 @@ trait HasAuditLog
 {
     public static function bootHasAuditLog()
     {
-        static::created(function ($user) {
-            OnCreatedLoggerJob::dispatchSync($user);
-        });
+        $ignoreActions = collect([]);
+        if (method_exists(self::class, 'ignoreAuditActions')) {
+            $ignoreActions = collect((new self)->ignoreAuditActions());
+        }
 
-        static::updated(function ($user) {
-            $ignoreFields = [];
-            if (property_exists(self::class, 'ignore_auditing')) {
-                $ignoreFields = (new self)->ignore_auditing;
-            }
-            OnUpdateLoggerJob::dispatchSync($user, $ignoreFields);
-        });
+        $isIgnoreCreateAction = self::isActionIgnored($ignoreActions, AuditAction::CREATE());
+        if (!$isIgnoreCreateAction) {
+            static::created(function ($user) {
+                OnCreatedLoggerJob::dispatchSync($user);
+            });
+        }
 
-        static::deleted(function ($user) {
-            OnDeletedLoggerJob::dispatchSync($user);
+        $isIgnoreUpdateAction = self::isActionIgnored($ignoreActions, AuditAction::UPDATE());
+        if (!$isIgnoreUpdateAction) {
+            static::updated(function ($user) {
+                $ignoreFields = [];
+                if (property_exists(self::class, 'ignore_auditing')) {
+                    $ignoreFields = (new self)->ignore_auditing;
+                }
+                OnUpdateLoggerJob::dispatchSync($user, $ignoreFields);
+            });
+        }
+
+        $isIgnoreDeleteAction = self::isActionIgnored($ignoreActions, AuditAction::DELETE());
+        if (!$isIgnoreDeleteAction) {
+            static::deleted(function ($user) {
+                OnDeletedLoggerJob::dispatchSync($user);
+            });
+        }
+    }
+
+    /**
+     * @param  Collection  $ignoreActions
+     * @param  AuditAction  $searchAction
+     * @return bool
+     */
+    protected static function isActionIgnored(Collection $ignoreActions, AuditAction $searchAction): bool
+    {
+        return $ignoreActions->contains(function (AuditAction $action) use ($searchAction) {
+            return $action->getValue() === $searchAction->getValue();
         });
     }
 }
