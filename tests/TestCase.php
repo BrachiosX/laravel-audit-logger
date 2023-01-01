03aa1270747c283369ee7efb76789a03133adc3f
Liam <zawhlyan@gmail.com>
2023-01-02 02:17:16 +0630

    feat: configure test environment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace BrachiosX\AuditLogger\Tests;
 
 use BrachiosX\AuditLogger\AuditLoggerServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Schema\Blueprint;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -13,7 +14,8 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'BrachiosX\\AuditLogger\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn(string $modelName
+            ) => 'BrachiosX\\AuditLogger\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
@@ -26,11 +28,46 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
+        $this->setUpDatabase($app);
+    }
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-audit-logger_table.php.stub';
+    protected function setUpDatabase($app)
+    {
+        $this->setUpTestingDBEnvironment();
+
+        $this->migrateAuditLogTable();
+
+        $this->migrateUserTable($app);
+    }
+
+    protected function setUpTestingDBEnvironment()
+    {
+        config()->set('database.default', 'sqlite');
+        config()->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    protected function migrateUserTable($app)
+    {
+        $app['db']->connection()->getSchemaBuilder()->create('users', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * @return void
+     */
+    protected function migrateAuditLogTable(): void
+    {
+        $migration = include __DIR__.'/../database/migrations/create_audit_logger_table.php.stub';
         $migration->up();
-        */
     }
 }
