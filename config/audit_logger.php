a4edaa18e4951496e39073f29c74bbeeb9d5082f
Liam <zawhlyan@gmail.com>
2023-01-08 17:52:53 +0630

    feat: add database configuration

--- config/audit_logger.php
+++ config/audit_logger.php
@@ -2,6 +2,9 @@
 
 // config for BrachiosX/AuditLogger
 return [
-    'table_name' => 'laravel_audit_logger_table',
-    'ignore_fields' => ['updated_at'],
+    'database' => [
+        'connection' => '',
+        'table_name' => 'laravel_audit_logger_table'
+    ],
+    'audit_ignore_fields' => ['updated_at'],
 ];
